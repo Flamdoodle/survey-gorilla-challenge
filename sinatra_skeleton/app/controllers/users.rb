0ac8f8eca5d4e621841915868ae4cb2f2d323e2c
Stephen Roth & Pei Liu <pair+flamdoodle.girb0t@devbootcamp.com>
2014-05-23 17:25:43 -0400

    merge conflict resolution. - logs user in on sign up.

--- sinatra_skeleton/app/controllers/users.rb
+++ sinatra_skeleton/app/controllers/users.rb
@@ -1,20 +1,47 @@
 get '/' do
 # Login and Sign up UNLESS loggedin
 # Redirect to profile page IF loggedin
-  erb :index
+  if session[:user_id]
+    redirect "/users/#{session[:user_id]}"
+  else
+    erb :index
+  end
 end
 
 post '/login' do
-# Validate user info.
-# Redirect to profile page.
+  @username = params[:username]
+  user = User.authenticate(@username, params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect "/users/#{user.id}"
+  else
+    @error = "invalid username or password."
+    erb :index
+  end
 end
 
-post 'signup' do
+post '/signup' do
+  user = User.new(username: params[:username])
+  user.password = params[:password]
+  if user.save
+    session[:user_id] = user.id
+    redirect "/users/#{user.id}"
+  else
+    redirect '/'
+  end
+
 # Create new user in database. Validate if exists.
 # Redirect to profile page.
 end
 
-get 'users/:id' do
+get '/users/:id' do
 ## Profile page shows User's Surveys
 ## and link to Survey create screen.
+  @user = User.find(params[:id])
+  erb :profile
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
