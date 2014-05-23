bb296615055d50179879f862861dbb4a69bebc10
Stephen Roth & Pei Liu <pair+flamdoodle.girb0t@devbootcamp.com>
2014-05-23 15:25:48 -0400

    -Add model and migration for User. -Include bcrypt in user model

---
+++ sinatra_skeleton/app/controllers/users.rb
@@ -0,0 +1,20 @@
+get '/' do
+# Login and Sign up UNLESS loggedin
+# Redirect to profile page IF loggedin
+  erb :index
+end
+
+post '/login' do
+# Validate user info.
+# Redirect to profile page.
+end
+
+post 'signup' do
+# Create new user in database. Validate if exists.
+# Redirect to profile page.
+end
+
+get 'users/:id' do
+## Profile page shows User's Surveys
+## and link to Survey create screen.
+end
