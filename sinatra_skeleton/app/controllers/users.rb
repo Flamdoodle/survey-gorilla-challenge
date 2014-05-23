ee3f5d3ea6064e9d1fe96c6c809f3895c209ff4e
Stephen Roth & Pei Liu <pair+flamdoodle.girb0t@devbootcamp.com>
2014-05-23 17:05:49 -0400

    Login/logout functionality working

--- sinatra_skeleton/app/controllers/users.rb
+++ sinatra_skeleton/app/controllers/users.rb
@@ -1,19 +1,30 @@
 get '/' do
 # Login and Sign up UNLESS loggedin
 # Redirect to profile page IF loggedin
-  erb :index
+  if session[:user_id]
+    redirect "/users/#{session[:user_id]}"
+  else
+    erb :index
+  end
 end
 
 post '/login' do
-# Validate user info.
-# Redirect to profile page.
+  @username = params[:username]
+  user = User.authenticate(@username, params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect "/users/#{user.id}"
+  else
+    @error = "invalid username or password."
+    erb :index
+  end
 end
 
 post '/signup' do
   user = User.new(username: params[:username])
   user.password = params[:password]
   user.save
-  redirect '/users/user.id'
+  redirect "/users/#{user.id}"
 # Create new user in database. Validate if exists.
 # Redirect to profile page.
 end
@@ -21,4 +32,11 @@ end
 get '/users/:id' do
 ## Profile page shows User's Surveys
 ## and link to Survey create screen.
+  @user = User.find(params[:id])
+  erb :profile
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
